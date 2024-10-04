2a8a586723f05fdc4948cc1e267b6a98ccba7f2c
iboigues <ig.boigues@gmail.com>
2024-10-04 21:59:21 +0200

    Charged jump done Need twealing to make a goodd feal En que momemento me he quedado dormido en mi cama y donde esta mi movil?

--- Assets/Code/Player/Jump.cs
+++ Assets/Code/Player/Jump.cs
@@ -1,48 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CapsuleCollider2D))]
 public class Jump : MonoBehaviour {
 
-    /*TODO: 
-    *   - Charged jump
-    *   - Ease in and out jump
-    */
-
     [SerializeField] private Transform _groundCheck;
-    [SerializeField] private float _jumpForce = 400f;
+    [SerializeField] private float _jumpForce = 5f;
 
     private Rigidbody2D _body;
+
     private readonly float k_GroundedRadius = 0.2f;
-    private bool _grounded;
+    [SerializeField] private bool _grounded;
 
     // Start is called before the first frame update
     private void Start() {
         _body = GetComponent<Rigidbody2D>();
     }
 
-    // Called 50 times per second, FixedUpdate its used for physic process
-    private void FixedUpdate() {
-        _grounded = false;
+    private void Update() {
 
-        Collider2D[] collider2Ds = Physics2D.OverlapCircleAll(_groundCheck.position,k_GroundedRadius);
+        _grounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheck.position,k_GroundedRadius);
 
         // Check if the player is touching ground
-        foreach (Collider2D collider2D in collider2Ds){
-            if(collider2D.gameObject != this.gameObject)
+        for(int i = 0;i < colliders.Length && !_grounded;i++){
+            if(colliders[i].gameObject != this.gameObject)
                 _grounded = true;
         }
-
+        
         HandleJump();
     }
 
     private void HandleJump(){
-        // Add a vertical force to the player.
         if(Input.GetKeyDown(KeyCode.Space) && _grounded){
 		    _grounded = false;
-		    _body.AddForce(new Vector2(0f, _jumpForce));
+		    _body.velocity = new Vector2(_body.velocity.x,_jumpForce);
         }
+
+        if(Input.GetKeyUp(KeyCode.Space) && _body.velocity.y > 0)
+            _body.velocity = new Vector2(-_body.velocity.x,0);
     }
 }
