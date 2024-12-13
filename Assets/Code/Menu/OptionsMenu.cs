6b90e813ae3f1f7145fa3e7233ac81516fc9008a
iboigues <ig.boigues@gmail.com>
2024-12-13 20:49:14 +0100

    Varios fixes - El jugador ya no se queda pillado por la cara - La caja ahora se pilla pasando por encima - Los botones de accesibilidad ahora funcionan en todos lados - Cambiado el sistema de guardado de barra de mutaciones

---
+++ Assets/Code/Menu/OptionsMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+    [SerializeField] private Toggle fallDamage;
+    [SerializeField] private Toggle autoShoot;
+
+    private void Start(){
+        fallDamage.isOn = GameManager.Instance.fallDamage; 
+        autoShoot.isOn = GameManager.Instance.autoShoot;
+    }
+
+    public void ToggleAutoShoot(bool value){
+        GameManager.Instance.autoShoot = !GameManager.Instance.autoShoot;
+    }
+
+    public void ToggleFallDamage(bool value){
+        GameManager.Instance.fallDamage = !GameManager.Instance.fallDamage;
+    }
+
+}
--- Assets/Code/Scene Scripts/MutationBar.cs
+++ Assets/Code/Scene Scripts/MutationBar.cs
@@ -4,40 +4,31 @@ using UnityEngine;
 
 public class MutationBar : MonoBehaviour
 {
-    public float currentCharge = 0f;
     public float maxCharge = 100f;
 
     [SerializeField] private Player player; // Arrastra tu jugador desde el Inspector
+    private PlayerData playerData;
     public MutationBarUi mutationBarUi; // Referencia a la UI
-
-    private static MutationBar instance; // Singleton para preservar el estado
 
     void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject); // Este objeto no se destruirá al cambiar de escena
-        }
-        else
-        {
-            Destroy(gameObject); // Evitar duplicados
-        }
     }
 
     void Start()
     {
-        mutationBarUi.UpdateBar(currentCharge, maxCharge); // Asegura que la UI refleje el progreso actual
+        playerData = player.playerData;
+        mutationBarUi.UpdateBar(playerData.currentCharge, maxCharge); // Asegura que la UI refleje el progreso actual
+        AddCharge(0);
     }
 
     public void AddCharge(float amount)
     {
-        currentCharge += amount;
-        mutationBarUi.UpdateBar(currentCharge, maxCharge);
+        playerData.currentCharge += amount;
+        mutationBarUi.UpdateBar(playerData.currentCharge, maxCharge);
 
-        if (currentCharge >= maxCharge)
+        if (playerData.currentCharge >= maxCharge)
         {
-            currentCharge = maxCharge;
+            playerData.currentCharge = maxCharge;
 
             if (player != null)
             {
@@ -48,7 +39,7 @@ public class MutationBar : MonoBehaviour
 
     public void ResetCharge()
     {
-        currentCharge = 0f;
-        mutationBarUi.UpdateBar(currentCharge, maxCharge);
+        playerData.currentCharge = 0f;
+        mutationBarUi.UpdateBar(playerData.currentCharge, maxCharge);
     }
 }
